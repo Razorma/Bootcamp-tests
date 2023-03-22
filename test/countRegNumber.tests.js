1ca9bd50dc942e9d1b94a15718ee2b6ae634ef8d
bheka <bheka.bj@gmail.com>
2023-03-22 07:56:08 +0200

    wrote tests for my first four functions

---
+++ test/countRegNumber.tests.js
@@ -0,0 +1,10 @@
+describe('Test My countRegNumber Function' , function(){
+    it("should return the number of registration numbers in the string" , function(){
+        assert.equal(3,countRegNumber('CA 182736,CY 523519,CJ 812328'));
+        assert.deepEqual(3,countRegNumber('CA 182736,CY 523519,CJ 812328'));
+        assert.equal(1,countRegNumber('CA 182736'));
+        assert.deepEqual(1,countRegNumber('CA 182736'));
+        assert.equal(5,countRegNumber('CA 182736,CY 523519,CJ 812328,DC 55 YU GP,ERT 123 EC'));
+        assert.deepEqual(5,countRegNumber('CA 182736,CY 523519,CJ 812328,DC 55 YU GP,ERT 123 EC'));
+    });
+});
